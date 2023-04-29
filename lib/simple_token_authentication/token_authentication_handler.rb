ce71aeccfe132e4cc7788de0f15d41ffbdaf144b
Ivan <anon@Ivans-iMac.local>
2023-04-29 13:46:42 -0700

    allow multiple emails

--- lib/simple_token_authentication/token_authentication_handler.rb
+++ lib/simple_token_authentication/token_authentication_handler.rb
@@ -57,10 +57,10 @@ module SimpleTokenAuthentication
       identifier_param_value = entity.get_identifier_from_params_or_headers(self).presence
 
       identifier_param_value = integrate_with_devise_case_insensitive_keys(identifier_param_value, entity)
-
+      token = entity.get_token_from_params_or_headers(self)
       # The finder method should be compatible with all the model adapters,
       # namely ActiveRecord and Mongoid in all their supported versions.
-      identifier_param_value && entity.model.find_for_authentication(entity.identifier => identifier_param_value)
+      identifier_param_value && entity.model.find_for_authentication(entity.identifier => identifier_param_value, , :authentication_token => token)
     end
 
     # Private: Take benefit from Devise case-insensitive keys
