4fb487902c594a5f279b9d89504a37962bedb623
Ivan <anon@Ivans-iMac.local>
2023-04-29 13:07:38 -0700

    test

--- lib/simple_token_authentication/token_authentication_handler.rb
+++ lib/simple_token_authentication/token_authentication_handler.rb
@@ -67,7 +67,10 @@ module SimpleTokenAuthentication
 
       # The finder method should be compatible with all the model adapters,
       # namely ActiveRecord and Mongoid in all their supported versions.
-      identifier_param_value && entity.model.find_for_authentication(entity.identifier => identifier_param_value, authentication_token => token)
+      puts "entity.identifier: #{entity.identifier.inspect}"
+      puts "identifier_param_value: #{identifier_param_value.inspect}"
+      puts "entity.model.find_for_authentication: #{entity.model.find_for_authentication.inspect}"
+      identifier_param_value && entity.model.find_for_authentication({entity.identifier => identifier_param_value, :authentication_token => token})
     end
 
     # Private: Take benefit from Devise case-insensitive keys
